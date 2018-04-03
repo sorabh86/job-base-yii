d066c48e07f1687169690e2038d78e25dbd1580f
sorabh sharma <ssorabh.ssharma@gmail.com>
2018-04-03 19:32:05 +0530

    yii2 init

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=job-base-yii',
+    'username' => 'job-base-yii',
+    'password' => 'job-base-yii',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
